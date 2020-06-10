863514dd019b364b89d36864282797eaed192db1
Franco Gastón Pellegrini <francogpellegrini@gmail.com>
2020-06-09 23:07:23 -0300

    installation fixes

--- install.sh
+++ install.sh
@@ -6,17 +6,20 @@ fc-cache -vf;
 sudo apt install -y fzf;
 if [[ $? > 0 ]]
 then
-    echo "fzf not found, trying to manually installation..."
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf || exit 100
-    ~/.fzf/install || exit 100
+    if [ ! -d "${HOME}/.fzf" ] 
+    then
+        echo "fzf not found, trying to manually installation..."
+        git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf || exit 100
+        ${HOME}/.fzf/install || exit 100
+    fi
 fi
 
 cd ${HOME};
 sudo rm -rf ${HOME}/.oh-my-zsh || exit 100;
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || exit 100;
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k || exit 100;
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || exit 100;
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || exit 100;
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/themes/powerlevel10k || exit 100;
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || exit 100;
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || exit 100;
 rsync -ahzc ${HOME}/.myzsh/.p10k.zsh ${HOME}/
 rsync -ahzc ${HOME}/.myzsh/.zshrc ${HOME}/
 
