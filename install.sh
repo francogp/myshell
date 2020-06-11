6c5d2c2d5eb81b6a4acf8fa1ae4fde4fff7a1068
Franco Gastón Pellegrini <francogpellegrini@gmail.com>
2020-06-11 09:07:46 -0300

    installation bugfixes for old ubuntu

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 OLD_UBUNTU=false
 if hostnamectl | grep -q "18.04"; then
-  echo "Old ubuntu detected, applying fixes"
+  echo "Old ubuntu detected"
   OLD_UBUNTU=true
 fi
 
@@ -11,11 +11,15 @@ sudo apt update && sudo apt install -y zsh rsync fonts-powerline ruby ruby-dev &
 
 # fix for old ubuntu fzf
 if [ "${OLD_UBUNTU}" = true ]; then
+  echo "* Applying old ubuntu patches"
   sudo gem update --system 3.0.6 || exit 100
+  sudo gem pristine rake || exit 100
   if [ ! -d "${HOME}/.fzf" ]; then
-    echo "fzf not found, trying to manually installation..."
+    echo "* fzf not found, trying to manually installation..."
     git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}"/.fzf || exit 100
     "${HOME}"/.fzf/install || exit 100
+  else
+    echo "* fzf already installed.. ignoring"
   fi
 else
   sudo apt install -y fzf || exit 100
