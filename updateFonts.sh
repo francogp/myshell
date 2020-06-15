fcc6a3c85df9ab49a80c60882dc8a5819378105b
Franco Gastón Pellegrini <francogpellegrini@gmail.com>
2020-06-15 18:27:46 -0300

    some fixes

--- updateFonts.sh
+++ updateFonts.sh
@@ -4,7 +4,8 @@ rm -rf ".myCache"
 mkdir -p ".myCache" || exit 100
 
 if grep -iq Microsoft /proc/version; then
-    echo "Ubuntu on Windows"   
+    echo " ==============DONE==================="
+    echo "Ubuntu on Windows"       
     echo "*** Install this fonts MANUALLY ***"
     echo "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf"
     echo "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf"
@@ -19,6 +20,7 @@ else
     mkdir -p "${HOME}/.local/share/fonts" || exit 100
     find "${HOME}/.myzsh/.myCache/" -type f -print0 | xargs -0 mv -t "${HOME}/.local/share/fonts"
     sudo fc-cache -f -v
+    echo " ==============DONE==================="
 fi
 
-echo "*** Configure terminal to use 'JetBrainsMono NF' font ***"
+echo "**** Configure terminal to use 'JetBrainsMono NF' font ****"
