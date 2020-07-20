2512342f086fe4d9552cf8edf9a823e81bf8505c
marcelo-ludovino <marcelo.docutec@gmail.com>
2020-07-20 11:03:00 -0300

    Inject updatem in zshrc

--- updatem.sh
+++ updatem.sh
@@ -7,7 +7,7 @@ echo "install nvm"
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 echo "install yarn"
 sudo npm i -g yarn
-' > update.sh
+' >> update.sh
 
 echo '
 alias pzshm='pzsh && bash ~/.myzsh/updatem.sh'
@@ -15,4 +15,4 @@ alias open='xdg-open'
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm 
-' > .zshrc
+' >> "${HOME}/.zshrc"
