d9a50c78d20ada91607ea2eebaaf898c7c6fadf4
marcelo-ludovino <marcelo.docutec@gmail.com>
2020-07-20 12:16:13 -0300

    Replace nuflo for brace home and and exit code to sentences

--- patch.install.sh
+++ patch.install.sh
@@ -2,10 +2,12 @@
 bash ~/.myzsh/install.sh
 
 echo "install nvm"
-sudo apt install xdg-utils
-echo "install nvm"
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+sudo apt install xdg-utils || exit 100
+echo "install nvm" 
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash || exit 100
 echo "install yarn"
-sudo npm i -g yarn
+sudo npm i -g yarn || exit 100
 
-bash ~/.myzsh/patch.update.sh
+echo "executing path update"
+bash "${HOME}/.myzsh/patch.update.sh" || exit 100
+echo "ending path update"
--- patch.zshrc.sh
+++ patch.zshrc.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-
+ 
 echo '
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-alias open='xdg-open'
-alias uzshm='bash ~/.myzsh/update.sh && upgrade_oh_my_zsh && bash ~/.myzsh/patch.update.sh && src'
-alias pzshm='cd ~/.myzsh && git pull && bash ~/.myzsh/update.sh  && bash ~/.myzsh/patch.update.sh && upgrade_oh_my_zsh && src'
-' >> ${HOME}/.zshrc'
+alias open='\''xdg-open'\''
+alias uzshm='\''bash ~/.myzsh/update.sh && upgrade_oh_my_zsh && bash ~/.myzsh/patch.update.sh && src'\''
+alias pzshm='\''cd ~/.myzsh && git pull && bash ~/.myzsh/update.sh  && bash ~/.myzsh/patch.update.sh && upgrade_oh_my_zsh && src'\''
+' >> "${HOME}/.zshrc"
