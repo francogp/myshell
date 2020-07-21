d5fe231f983cb0a4d19cddc53356415784299e62
Franco Gastón Pellegrini <francogpellegrini@gmail.com>
2020-07-21 20:11:22 -0300

    Update patch.install.sh
    possible fix for install

--- patch.install.sh
+++ patch.install.sh
@@ -3,14 +3,17 @@ bash ~/.myzsh/install.sh
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+PATH="$HOME/bin:$HOME/.local/bin:/usr/bin:$PATH"
 
-echo "install nvm"
+echo "* Install npm"
+sudo apt-get install -y nodejs build-essential npm || exit 100
+echo "* Install nvm"
 sudo apt install xdg-utils || exit 100
-echo "install nvm" 
+echo "* Install nvm" 
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash || exit 100
-echo "install yarn"
+echo "* Install yarn"
 sudo npm i -g yarn || exit 100
 
-echo "executing path update"
+echo "* Executing path update"
 bash "${HOME}/.myzsh/patch.update.sh" || exit 100
 echo "ending path update"
