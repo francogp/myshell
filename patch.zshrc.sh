aa16fbfdfdc0f1b463b20bccaaae8c585ad6a083
MarceloLudovino <ext-m.ludovino@gmail.com>
2020-10-10 22:18:11 -0300

    Add slack, jetbrains, docker, chrome

--- patch.zshrc.sh
+++ patch.zshrc.sh
@@ -6,4 +6,5 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 alias open='\''xdg-open'\''
 alias uzshm='\''bash ~/.myzsh/update.sh && omz update && bash ~/.myzsh/patch.update.sh && src'\''
 alias pzshm='\''cd ~/.myzsh && git pull && bash ~/.myzsh/update.sh  && bash ~/.myzsh/patch.update.sh && omz update && src'\''
+alias cdc='\''cd ~/code'\''
 ' >> "${HOME}/.zshrc"
