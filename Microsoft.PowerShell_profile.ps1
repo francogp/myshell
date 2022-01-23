e2ae9a45543fe6676bf094de120873fd62a295ae
Franco Gastón Pellegrini <francogpellegrini@gmail.com>
2022-01-23 14:12:57 -0300

    windwos installation and update fixes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,6 @@
 $env:STARSHIP_DISTRO = "";
 $env:IP = (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content;
 
+Set-Alias "ushell" "cd ~/.myzsh && git fetch origin && git reset --hard origin/master && pwsh update.ps1"
+
 Invoke-Expression (&starship init powershell)
--- install.ps1
+++ install.ps1
@@ -1,26 +1,20 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
+    Start-Process pwsh.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit 
+}
+
+# 1) INSTALL powershell from microsoft store!
+# 2) install nerdfonts!
 
 Write-Output "Installing chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-Write-Output "Restart terminal, and execute this script again"
-choco upgrade chocolatey -y
-choco install starship --force -y
+if (!(Test-Path -Path $PROFILE)) {
+    New-Item -ItemType File -Path $PROFILE -Force
+}
 
-if (!(Test-Path $PROFILE)) {
-    Write-Host "Creating config file to support starship on powershell...";
-    New-Item -path (Split-Path -Path $PROFILE) -name (Split-Path -Path $PROFILE -Leaf) -type "file" -value "Invoke-Expression (&starship init powershell)" -Force;
-}
-else {
-    $SEL = Select-String -Path $PROFILE -Pattern "starship init powershell";
-    if ($null -ne $SEL) {
-        Write-Host "already configurated.. ignoring code";
-    }
-    else {
-        Write-Host "Updating config file to support starship on powershell...";
-        Add-Content -path $PROFILE -value "`r`nInvoke-Expression (&starship init powershell)" -Force;
-    }
-}
+Write-Output "Restart terminal, and execute this script again"
+choco upgrade all -y
+choco install starship --force -y
 
 New-Item -Path (Split-Path -Path $PROFILE) -type "directory" -Force;
 Copy-Item -Path "$PSScriptRoot\Microsoft.PowerShell_profile.ps1" -Destination $PROFILE -Force
